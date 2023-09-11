b1e3690ae33693787e2869b376d317a8eed35990
vitkai <39426330+vitkai@users.noreply.github.com>
2023-09-12 00:11:21 +0300

    date input refactored

--- bare/src/components/Dateinput.tsx
+++ bare/src/components/Dateinput.tsx
@@ -1,28 +1,19 @@
-import { createSignal } from "solid-js";
+import { Accessor, Component, Setter, createSignal } from "solid-js";
 
-const [date, setDate] = createSignal("");
-const [result, setResult] = createSignal<string>("");
+interface DateInputProps {
+  date: Accessor<string>,
+  setDate: Setter<string>
+}
 
-const processDate = () => {
-  const inputDate = date();
-  // Perform your calculations on the input date and update the result
-  // For this example, we'll just display the input date as is.
-  setResult(`You entered: ${inputDate}`);
-};
-
-export default function Dateinput() {
+export const DateInput: Component<DateInputProps> = (props) => {
   return (
     <div>
       <label>Date:</label>
       <input
         type="text"
-        value={date()}
-        onInput={(e) => setDate(e.target.value)}
+        value={props.date()}
+        onInput={(e) => props.setDate(e.target.value)}
       />
-      <button onClick={processDate }>Calculate</button>
-      <div>
-        <p>Result: {result()}</p>
-      </div>
     </div>
   );
 }
--- bare/src/routes/index.tsx
+++ bare/src/routes/index.tsx
@@ -1,6 +1,17 @@
 import { Title } from "solid-start";
 import Counter from "~/components/Counter";
-import Dateinput from "~/components/Dateinput";
+import { DateInput } from "~/components/Dateinput";
+import { createSignal } from "solid-js";
+
+const [date, setDate] = createSignal("");
+const [result, setResult] = createSignal<string>("");
+
+const processDate = () => {
+  const inputDate = date();
+  // Perform your calculations on the input date and update the result
+  // For this example, we'll just display the input date as is.
+  setResult(`You entered: ${inputDate}`);
+};
 
 export default function Home() {
   return (
@@ -8,7 +19,11 @@ export default function Home() {
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
       <Counter />
-      <Dateinput />
+      <DateInput date={date} setDate={setDate} />
+      <button onClick={processDate }>Calculate</button>
+      <div>
+        <p>Result: {result()}</p>
+      </div>
     </main>
   );
 }
