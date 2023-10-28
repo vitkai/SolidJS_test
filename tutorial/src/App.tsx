3d9017d47e5a593558de18dd5ebb2de1dce407a1
vitkai <39426330+vitkai@users.noreply.github.com>
2023-10-28 11:24:43 +0300

    BookshelfProps added

--- tutorial/src/App.tsx
+++ tutorial/src/App.tsx
@@ -1,17 +1,20 @@
 import { BookList } from "./components/BookList";
 import { AddBook } from "./components/AddBook";
-function Bookshelf() {
+interface BookshelfProps {
+  name: string;
+}
+function Bookshelf(props: BookshelfProps) {
   return (
     <div>
-      <h1>My Bookshelf</h1>
+      <h1>{props.name}'s Bookshelf</h1>
       <BookList />
       <AddBook />
     </div>
-  )
+  );
 }
 function App() {
   return (
-    <Bookshelf/>
+    <Bookshelf name="solid"/>
   );
 }
 export default App;
